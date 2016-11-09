43232029b528fd528d5b6991ac8a43bc0a8be656
CatArthur <mssizov@gmail.com>
2016-11-09 18:21:18 +0300

    push

--- src/First.java
+++ src/First.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class First {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
         System.out.println("Hello, World!");
+        System.out.println("sdg");
 
     }
 }
